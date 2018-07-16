6da203a04703be777b8a65e412ce416a68ebc81f
meiysh <meiysh@tingshan.cn>
2018-07-16 17:49:14 +0800

    #1 Action Begin

---
+++ PR200 MKll/PR200 MKll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PR200 MKll
+//
+//  Created by 梅 on 2018/7/16.
+//  Copyright © 2018年 mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PR200 MKll/PR200 MKll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PR200 MKll
+//
+//  Created by 梅 on 2018/7/16.
+//  Copyright © 2018年 mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
